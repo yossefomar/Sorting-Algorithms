b4ddee0cba21984e6ef5c8958eaf00e9dd4f133f
yossefomar <yossefomar931@gmail.com>
2022-05-09 00:29:18 +0200

    Updated

--- merge sort/merge sort/Program.cs
+++ merge sort/merge sort/Program.cs
@@ -9,17 +9,7 @@ namespace merge_sort
             int[] arr = {9,8,7,6,5,4,3,2,1};
             int s = 0;
             int e = (arr.Length-1);
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write(arr[i]);
-            }
-            Console.WriteLine();
             MS(arr, s, e);
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write(arr[i]);
-            }
-            
         }
 
 
@@ -46,29 +36,18 @@ namespace merge_sort
         {
             int[] left = new int[(mid+1) - s];
             int j = 0;
-            for (int i = mid + 1; i <= e; i++)
+            for (int i = s; i <= mid; i++)
             {
                 left[j] = A[i];
                 j++;
             }
 
-
-
-
-            for (int i = s; i < e; i++)
-            {
-                Console.Write(A[i]);
-            }
-            Console.WriteLine();
-
-
-
             //merging
             int pointer1 = mid+1;
             int pointer2 = 0;
-            for (int i = s; i <e; i++)
+            for (int i = s; i <=e; i++)
             {
-                if (pointer1 == e)
+                if (pointer1 > e)
                 {
                     A[i] = left[pointer2];
                     pointer2++;
@@ -90,12 +69,6 @@ namespace merge_sort
                 }
             }
 
-
-            for (int i = s; i < e; i++)
-            {
-                Console.Write(A[i]);
-            }
-            Console.WriteLine();
             //return sorted array
         }
     }
